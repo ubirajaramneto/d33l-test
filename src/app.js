51f4d2ee0a4be732a54af9be8b72f02dbb63be87
Ubirajara Marsicano Neto <ubirajara_neto@hotmail.com>
2022-08-22 18:48:49 -0300

    feat(jobs): add endpoint to get unpaid jobs for a contract

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { sequelize, Contract } = require("./model");
+const { sequelize, Contract, Job } = require("./model");
 const { getProfile } = require("./middleware/getProfile");
 const { Op } = require("sequelize");
 const app = express();
@@ -33,6 +33,23 @@ app.get("/contracts", getProfile, async (req, res) => {
       ],
       status: { [Op.not]: "terminated" },
     },
+  });
+  res.json(contracts);
+});
+
+// GET /jobs/unpaid - Get all unpaid jobs for a user (either a client or contractor), for active contracts only.
+app.get("/jobs/unpaid", getProfile, async (req, res) => {
+  const currentProfileId = req.profile.dataValues.id;
+  const contracts = await Contract.findAll({
+    where: {
+      [Op.or]: [
+        { contractorId: currentProfileId },
+        { clientId: currentProfileId },
+      ],
+      status: { [Op.not]: "terminated" },
+      '$Jobs.paid$': {[Op.eq]: null}
+    },
+    include: [{model: Job, as:'Jobs'}],
   });
   res.json(contracts);
 });
