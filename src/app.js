cd616d29dc2f19616c1cacfcbc8167a8cb312d75
Ubirajara Marsicano Neto <ubirajara_neto@hotmail.com>
2022-08-22 18:35:50 -0300

    feat(contracts): add endpoint for getting all contracts for a profile

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('./model')
+const {sequelize, Contract} = require('./model')
 const {getProfile} = require('./middleware/getProfile')
+const {Op} = require("sequelize");
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
@@ -12,12 +13,20 @@ app.set('models', sequelize.models)
  * @returns contract by id
  */
 app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const currentProfile = req.profile.dataValues.id;
+    const currentProfileId = req.profile.dataValues.id;
     const {Contract} = req.app.get('models')
     const {id} = req.params
     const contract = await Contract.findOne({where: {id}})
-    if (currentProfile !== contract.contractorId) return res.status(401).end()
+    if (currentProfileId !== contract.contractorId) return res.status(401).end()
     if(!contract) return res.status(404).end()
     res.json(contract)
 })
+
+// Returns a list of contracts belonging to a user (client or contractor), the list should only contain non terminated contracts.
+app.get('/contracts', getProfile, async (req, res) => {
+    const currentProfileId = req.profile.dataValues.id;
+    const contracts = await Contract.findAll({where: {[Op.or]:[{contractorId: currentProfileId},{clientId: currentProfileId}], status: {[Op.not]: 'terminated'}}})
+    res.json(contracts);
+})
+
 module.exports = app;
