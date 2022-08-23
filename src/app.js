38f5d676647f410a8cc69a2bfb4d0788cc964321
Ubirajara Marsicano Neto <ubirajara_neto@hotmail.com>
2022-08-23 13:33:39 -0300

    fix(test): fixed a bug for the 404 condition, added more tests for the contracts endpoint

--- src/app.js
+++ src/app.js
@@ -64,8 +64,8 @@ app.get("/contracts/:id", getProfile, async (req, res) => {
   const { Contract } = req.app.get("models");
   const { id } = req.params;
   const contract = await Contract.findOne({ where: { id } });
-  if (currentProfileId !== contract.ContractorId) return res.status(401).end();
   if (!contract) return res.status(404).end();
+  if (currentProfileId !== contract?.ContractorId) return res.status(401).end();
   res.json(contract);
 });
 
--- src/test.js
+++ src/test.js
@@ -1,16 +1,50 @@
-const request = require('supertest');
-const assert = require('assert');
-const express = require('express');
-const app = require('./app');
+const request = require("supertest");
+const assert = require("assert");
+const express = require("express");
+const app = require("./app");
 
-describe('Contracts', () => {
-  test('it should not allow for an unauthenticated user to get a contract', (done) => {
+describe("Contracts", () => {
+  test("it should not allow for an unauthenticated user to get a contract", (done) => {
     request(app)
-      .get('/contracts/2')
+      .get("/contracts/2")
       .expect(401)
-      .end(function(err, res) {
-        if (err) done(err);
-        done()
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
       });
-  })
-})
+  });
+
+  test("it should not allow for a profile that is not part of a contract to get a contract", (done) => {
+    request(app)
+      .get("/contracts/2")
+      .set("profile_id", "2")
+      .expect(401)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  test("it should not allow for an invalid contract to be returned", (done) => {
+    // this was a good test, it caught a bug \o/
+    request(app)
+      .get("/contracts/99212987")
+      .set("profile_id", "2")
+      .expect(404)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+
+  test("it should allow for a profile to get a contract they are part of", (done) => {
+    request(app)
+      .get("/contracts/2")
+      .set("profile_id", "6")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        return done();
+      });
+  });
+});
